c8d30dfe56baae529e71685841ad9ed5f6557ec4
Gabriel <gabrieldasilvaprivado1@gmail.com>
2021-10-02 03:50:15 -0300

    Send project Social Network with NextJS and ReactJS
    Projeto da Alura Imersao React

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,184 @@
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
+import MainGrid from '../src/components/MainGrid';
+import Box from '../src/components/Box';
+import { AlurakutMenu, AlurakutProfileSidebarMenuDefault, OrkutNostalgicIconSet } from '../src/lib/SocialCommons';
+import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+function ProfileSideBar (props) {
+  return (
+    <Box as="aside">
+      <img src={`https://github.com/${props.gitUser}.png`} style={{ borderRadius: '8px' }} />
+      <hr />
+
+      <a className="boxLink" href={`https://github.com/${props.gitUser}`}>
+        @{props.gitUser}
+      </a>
+      <hr />
+
+    <AlurakutProfileSidebarMenuDefault />
+    </Box>
+  )
+}
+
+function ProfileRelationsBox(props) {
+  return(
+    <ProfileRelationsBoxWrapper>
+      <h2 className="smallTitle"> 
+        {props.title} ({props.items.length})
+      </h2>
+      <ul>
+          {/* {followers.map((item) => {
+            return (
+              <li key={item}>
+                <a href={`https://www.github.com/${item}.png`}>
+                  <img src={item} />
+                  <span>{item}</span>
+                </a>
+              </li>
+            )
+          })} */}
+        </ul>
+    </ProfileRelationsBoxWrapper>
+  )
+}
 
 export default function Home() {
-  return <Title>My page</Title>
+  const gitUser = 'gabrieldasilvadev';
+  const [community, setCommunity] = React.useState([{
+    id: '452353256',
+    title: 'Amantes de React',
+    image: 'https://reactjs.org/logo-og.png'
+  }]);
+  console.log(community);
+  const favoritePeople = [
+    'marcobrunodev',
+    'rafaballerini',
+    'gabrieldasilvadev',
+    'filipedeschamps',
+    'JeCarlos7713',
+    'rafaelmaiach',
+  ];
+
+  const [followers, setFollowers] = React.useState([])
+
+  //  0 - Pegar o array de dados do GitHub
+  React.useEffect(function () {
+  fetch('https://api.github.com/users/gabrieldasilvadev/followers')
+    .then(function(respostaDoServidor){
+        return respostaDoServidor.json();
+      
+    })
+    .then(function(respostaCompleta){
+      setFollowers(respostaCompleta);
+    })
+  }, [])
+
+
+  // 1 - Criar um box que vai ter um map, baseado nos itens do array que pegamos do GitHub.
+
+
+  fetch('https://api.github.com/users/gabrieldasilvadev/followers')
+
+  
+
+  return (
+    <>
+    <AlurakutMenu />
+    <MainGrid>
+      <div className="profileArea" style={{gridArea: 'profileArea'}}>
+          <ProfileSideBar gitUser={gitUser}/>
+      </div>
+
+      <div className="welcomeArea" style={{gridArea: 'welcomeArea'}}>
+        <Box>
+          <h1 className="title">
+            Welcome!
+            <p className="welcomeUser">@{gitUser}</p>
+          </h1>
+          
+          <OrkutNostalgicIconSet/>
+        </Box>
+
+        <Box>
+          <h2 className="subTitle">O que voce deseja fazer agora?</h2>
+          <form onSubmit={function hundleCreateCommunity(e) {
+            e.preventDefault();
+            const dataForm = new FormData(e.target);
+
+            console.log(dataForm.get('title'));
+            console.log(dataForm.get('image'));
+
+            const myCommunity = {
+              id: new Date().toISOString(),
+              title: dataForm.get('title'),
+              image: dataForm.get('image'),
+            }
+            const newCommunity = [...community, myCommunity]
+            setCommunity(newCommunity)
+          }}>
+            <div>
+              <input placeholder="Qual vai ser o seu nome na comunidade?" 
+              name="title" 
+              aria-label="Qual vai ser o seu nome na comunidade?"
+              type="text"
+              />
+            </div>
+            <div>
+              <input placeholder="Coloque uma URL para usarmos de capa" 
+              name="image" 
+              aria-label="Coloque uma URL para usarmos de capa"
+              />
+            </div>
+
+            <button>
+              Create Community
+            </button>
+          </form>
+        </Box>
+      </div>
+
+      <div className="profileRelationsArea" style={{gridArea: 'profileRelationsArea'}}>
+
+        <ProfileRelationsBox title="Followers" items={followers} />
+
+        <ProfileRelationsBoxWrapper>
+          <h2 className="smallTitle">My Community ({community.length})</h2>
+          <ul>
+              {community.map((item) => {
+                return (
+                  <li key={item.id}>
+                    <a href={`/users/${item.title}`}>
+                      <img src={item.image} />
+                      <span>{item.title}</span>
+                    </a>
+                  </li>
+                )
+              })}
+            </ul>
+        </ProfileRelationsBoxWrapper>
+
+        <ProfileRelationsBoxWrapper>
+        <h2 className="smallTitle">
+          My Friends ({favoritePeople.length})
+        </h2>
+
+          <ul>
+            {favoritePeople.map((item) => {
+              return (
+                <li key={item}>
+                  <a 
+                  href={`https://www.github.com/${item}`}>
+                  <img src={`https://www.github.com/${item}.png`}/>
+                  <span>{item}</span>
+                  </a>
+                </li>
+              )
+            })}
+          </ul>
+        </ProfileRelationsBoxWrapper>
+      </div>
+    </MainGrid>
+    </>
+  )
 }
---
+++ src/components/Box/index.js
@@ -0,0 +1,76 @@
+import styled from 'styled-components'
+
+const Box = styled.div`
+background-color: rgb(41, 40, 65);
+border-radius: 8px;
+filter: drop-shadow(2px 2px 2px #111111);
+
+padding: 16px;
+
+margin-bottom: 10px;
+
+.boxLink {
+    font-size: 14px;
+    color: #2E7BB4;
+    text-decoration: none;
+    font-weight: 800;
+    display: flex;
+    justify-content: center;
+    align-content: center;
+  }
+.title {
+    font-size: 32px;
+    font-weight: 400;
+    margin-bottom: 20px;
+  }
+  .subTitle {
+    font-size: 18px;
+    font-weight: 400;
+    margin-bottom: 20px;
+  }
+  .smallTitle {
+    margin-bottom: 20px;
+    font-size: 16px;
+    font-weight: 700;
+    color: #F1F1F1;
+    margin-bottom: 20px;
+  }
+  hr {
+    margin-top: 12px;
+    margin-bottom: 8px;
+    border-color: transparent;
+    border-bottom-color: #ECF2FA;
+  }
+  input {
+    width: 100%;
+    background-color: hsla(261, 95%, 6%, 0.40);
+    color: #333333;
+    border: 0;
+    padding: 14px 16px;
+    margin-bottom: 14px;
+    border-radius: 10000px;
+    ::placeholder {
+      color: #F1F1F1;
+      opacity: 1;
+    }
+  }
+  button {
+    border: 0;
+    padding: 8px 12px;
+    color: #FFFFFF;
+    border-radius: 10000px;
+    background-color: #6F92BB;
+  }
+  .welcomeUser {
+    @media (min-width: 860px) {
+      display: none;
+    }
+    display: block;
+    font-size: 14px;
+    margin-top: 10px;
+    color: #2E7BB4;
+  }
+
+`;
+
+export default Box;
